2c46391062f3635ea966b42e4aa769cb5d07dbcd
The Shayds <shayds@hotmail.fr>
2018-08-21 11:28:14 +0200

    Add comments Add Help and About popup message

--- app/src/main/java/com/example/theshayds/mynewstest/Controller/MainActivity.java
+++ app/src/main/java/com/example/theshayds/mynewstest/Controller/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.theshayds.mynewstest.Controller;
 
-
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
@@ -16,15 +18,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
-
 import com.example.theshayds.mynewstest.R;
-import com.example.theshayds.mynewstest.Views.ArticleAdapter;
 import com.example.theshayds.mynewstest.Views.ArticleSearchFragment;
 import com.example.theshayds.mynewstest.Views.MostPopularFragment;
 import com.example.theshayds.mynewstest.Views.TopStoriesFragment;
 import com.example.theshayds.mynewstest.Views.ViewPagerAdapter;
-
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
     private ViewPager mViewPager;
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
-    ArticleAdapter mAdapter;
+    Dialog mDialog;
 
 
     @Override
@@ -41,53 +40,56 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Create new Dialog
+        mDialog = new Dialog(this);
+
+        // Create toolbar
         mToolbar = findViewById(R.id.toolbar);
         setSupportActionBar(mToolbar);
         final ActionBar mActionBar = getSupportActionBar();
 
+        // Create NavigationDrawer
         mDrawerLayout = findViewById(R.id.drawer_layout);
         mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         mDrawerLayout.addDrawerListener(mToggle);
         mToggle.syncState();
 
+        // Add menu button to Toolbar
         assert mActionBar != null;
         mActionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
         mActionBar.setDisplayHomeAsUpEnabled(true);
 
+        // Create and setup viewPager
         mViewPager = findViewById(R.id.viewPager);
         setupViewPager(mViewPager);
 
+        // Create and setup TabLayout with viewPager
         mTabLayout = findViewById(R.id.tabs);
         mTabLayout.setupWithViewPager(mViewPager);
 
+        // NavigationDrawer multiple events
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(
                 new NavigationView.OnNavigationItemSelectedListener() {
                     @Override
                     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                        // set item as selected to persist highlight
-                        menuItem.setChecked(true);
+                        // Set item as selected to persist highlight
+                        // menuItem.setChecked(true);
 
                         switch (menuItem.getItemId()) {
 
                             case R.id.nav_top_stories :{
                                 mViewPager.setCurrentItem(0);
-                                Toast toast = Toast.makeText(getApplicationContext(), "Top Stories", Toast.LENGTH_LONG);
-                                toast.show();
                                 break;
                                 }
 
                             case R.id.nav_most_popular :{
                                 mViewPager.setCurrentItem(1);
-                                Toast toast = Toast.makeText(getApplicationContext(), "Most Popular", Toast.LENGTH_LONG);
-                                toast.show();
                                 break;
                             }
 
                             case R.id.nav_business :{
                                 mViewPager.setCurrentItem(2);
-                                Toast toast = Toast.makeText(getApplicationContext(), "Business", Toast.LENGTH_LONG);
-                                toast.show();
                                 break;
                             }
                             case R.id.nav_search :{
@@ -103,13 +105,11 @@ public class MainActivity extends AppCompatActivity {
                             }
 
                             case R.id.nav_help :{
-                                Toast toast = Toast.makeText(getApplicationContext(), "Business", Toast.LENGTH_LONG);
-                                toast.show();
+                                showHelpPopup();
                                 break;
                             }
                             case R.id.nav_about :{
-                                Toast toast = Toast.makeText(getApplicationContext(), "Business", Toast.LENGTH_LONG);
-                                toast.show();
+                                showAboutPopup();
                                 break;
                             }
                         }
@@ -147,6 +147,22 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    // Show "help" information
+    private void showHelpPopup() {
+        mDialog.setContentView(R.layout.popup_help);
+        Objects.requireNonNull(mDialog.getWindow()).setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        mDialog.show();
+    }
+
+    // Show "about" information
+    private void showAboutPopup() {
+        mDialog.setContentView(R.layout.popup_about);
+        Objects.requireNonNull(mDialog.getWindow()).setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        mDialog.show();
+    }
+
+
+    // Close navigationDrawer when back button is pressed
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
@@ -155,10 +171,6 @@ public class MainActivity extends AppCompatActivity {
         } else {
             super.onBackPressed();
         }
-    }
-
-    public void onClearItems(){
-        mAdapter.clearNews();
     }
 
 
@@ -181,23 +193,25 @@ public class MainActivity extends AppCompatActivity {
         // Switch between items from toolbar menu
         switch (item.getItemId()){
             case R.id.search:{
+                // Start "SearchActivity"
                 Intent mIntent2 = new Intent(MainActivity.this, SearchActivity.class);
                 startActivity(mIntent2);
                 break;
             }
             case R.id.notifications:{
+                // Start "NotificationActivity"
                 Intent mIntent = new Intent(MainActivity.this, NotificationsActivity.class);
                 startActivity(mIntent);
                 break;
             }
             case R.id.help: {
-                Toast toast = Toast.makeText(getApplicationContext(), "Help", Toast.LENGTH_LONG);
-                toast.show();
+                // Open popup to show information "help"
+                showHelpPopup();
                 break;
             }
             case R.id.about: {
-                Toast toast = Toast.makeText(getApplicationContext(), "About", Toast.LENGTH_LONG);
-                toast.show();
+                // Open popup to show information "about"
+                showAboutPopup();
                 break;
             }
         }
@@ -205,6 +219,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void setupViewPager(ViewPager viewPager) {
+        // Setup the ViewPager with multiple fragment
         ViewPagerAdapter mPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
         mPagerAdapter.addFragment(new TopStoriesFragment(), "TOP STORIES");
         mPagerAdapter.addFragment(new MostPopularFragment(), "MOST POPULAR");
