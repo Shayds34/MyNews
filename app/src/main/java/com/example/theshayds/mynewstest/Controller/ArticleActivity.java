a90cebb6f659f1ad9d233e03dc8b852b920d8afe
The Shayds <shayds@hotmail.fr>
2018-08-21 11:29:51 +0200

    Add comments

--- app/src/main/java/com/example/theshayds/mynewstest/Controller/ArticleActivity.java
+++ app/src/main/java/com/example/theshayds/mynewstest/Controller/ArticleActivity.java
@@ -23,30 +23,26 @@ public class ArticleActivity extends AppCompatActivity {
         Toolbar mToolbar = findViewById(R.id.toolbar);
         setSupportActionBar(mToolbar);
 
-        // add back arrow to toolbar
+        // Add back arrow to toolbar
         if (getSupportActionBar() != null){
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setDisplayShowHomeEnabled(true);
         }
 
-
+        // Get the url of an article
         Intent mIntent = getIntent();
         String mURL = mIntent.getStringExtra("URL");
 
-
+        // Setup webView
         WebView mWebView = findViewById(R.id.webView);
-        // Enable JavaScript
-        // mWebView.getSettings().setJavaScriptEnabled(true);
         mWebView.setFocusable(true);
         mWebView.setFocusableInTouchMode(true);
-        // Set Render Priority to High
         mWebView.getSettings().setRenderPriority(WebSettings.RenderPriority.HIGH);
         mWebView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
         mWebView.getSettings().setDomStorageEnabled(true);
         mWebView.getSettings().setDatabaseEnabled(true);
         mWebView.getSettings().setAppCacheEnabled(true);
         mWebView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
-
 
         // Load URL
         mWebView.loadUrl(mURL);
@@ -55,9 +51,9 @@ public class ArticleActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // handle arrow click here
+        // Handle arrow click here
         if (item.getItemId() == android.R.id.home) {
-            finish(); // close this activity and return to preview activity (if there is any)
+            finish(); // Close this activity and return to preview activity (if there is any)
         }
         return super.onOptionsItemSelected(item);
     }
